2048a4a2082c2dd133a57642d213a696e4932260
Ruslan Galiev <galievruslan@gmail.com>
2020-05-05 14:23:02 +0300

    refactored DateSelector to func component

--- src/components/dateSelector/dateSelector.component.tsx
+++ src/components/dateSelector/dateSelector.component.tsx
@@ -1,39 +1,45 @@
 import moment from 'moment';
-import React, {PureComponent} from 'react';
+import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from './dateSelector.styles';
 
-interface DateSelectorProps {
+type DateSelectorProps = {
   type: 'isoWeek' | 'month' | 'year';
   date: moment.Moment;
   changeDate: (date: moment.Moment) => void;
-}
+};
 
-export default class DateSelector extends PureComponent<DateSelectorProps> {
-  render() {
-    return (
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          borderBottomWidth: 1,
-        }}>
-        <TouchableOpacity onPress={this.handleLeftButton}>
-          <Icon size={24} name="chevron-left" color="blue" />
-        </TouchableOpacity>
-        {this.renderDate()}
-        <TouchableOpacity onPress={this.handleRightButton}>
-          <Icon size={24} name="chevron-right" color="blue" />
-        </TouchableOpacity>
-      </View>
-    );
-  }
+export const DateSelector = (props: DateSelectorProps) => {
+  const {type, date, changeDate} = props;
 
-  renderDate() {
-    const {type} = this.props;
-    let date = this.props.date;
+  const handleRightButton = () => {
+    let newDate = moment(date);
 
+    if (type === 'isoWeek') {
+      newDate.add(1, 'week');
+    } else if (type === 'month') {
+      newDate.add(1, 'month');
+    } else if (type === 'year') {
+      newDate.add(1, 'year');
+    }
+    changeDate(newDate);
+  };
+
+  const handleLeftButton = () => {
+    let newDate = moment(date);
+
+    if (type === 'isoWeek') {
+      newDate.subtract(1, 'week');
+    } else if (type === 'month') {
+      newDate.subtract(1, 'month');
+    } else if (type === 'year') {
+      newDate.subtract(1, 'year');
+    }
+    changeDate(newDate);
+  };
+
+  const renderDate = () => {
     if (type === 'isoWeek') {
       return (
         <Text>
@@ -47,33 +53,16 @@ export default class DateSelector extends PureComponent<DateSelectorProps> {
     } else if (type === 'year') {
       return <Text>{date.format('YYYY')}</Text>;
     }
-  }
-
-  handleRightButton = () => {
-    let {type} = this.props;
-    let date = moment(this.props.date);
-
-    if (type === 'isoWeek') {
-      date.add(1, 'week');
-    } else if (type === 'month') {
-      date.add(1, 'month');
-    } else if (type === 'year') {
-      date.add(1, 'year');
-    }
-    this.props.changeDate(date);
   };
-
-  handleLeftButton = () => {
-    let {type} = this.props;
-    let date = moment(this.props.date);
-
-    if (type === 'isoWeek') {
-      date.subtract(1, 'week');
-    } else if (type === 'month') {
-      date.subtract(1, 'month');
-    } else if (type === 'year') {
-      date.subtract(1, 'year');
-    }
-    this.props.changeDate(date);
-  };
-}
+  return (
+    <View style={styles.mainContainer}>
+      <TouchableOpacity onPress={handleLeftButton}>
+        <Icon size={24} name="chevron-left" color="blue" />
+      </TouchableOpacity>
+      {renderDate()}
+      <TouchableOpacity onPress={handleRightButton}>
+        <Icon size={24} name="chevron-right" color="blue" />
+      </TouchableOpacity>
+    </View>
+  );
+};
--- src/components/dateSelector/dateSelector.styles.tsx
+++ src/components/dateSelector/dateSelector.styles.tsx
@@ -2,9 +2,10 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   mainContainer: {
-    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    borderBottomWidth: 1,
   },
 });
 
